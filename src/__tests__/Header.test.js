5da350606b64b411644846021687ea75d2140990
bornittah <bornittah@gmail.com>
2022-12-22 16:43:41 +0300

    Add unit testing

---
+++ src/__tests__/Header.test.js
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from '../features/Header/Header';
+
+describe('Header', () => {
+  it('renders Header component', async () => {
+    const tree = render(<Router><Header /></Router>);
+    expect(tree).toMatchSnapshot();
+  });
+});
